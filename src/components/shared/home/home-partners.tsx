20c5b611a4873f613b5f2e9e7de78c3a6661b4ec
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-11-27 19:04:49 +0500

    carousel added

--- src/components/shared/home/home-partners.tsx
+++ src/components/shared/home/home-partners.tsx
@@ -1,6 +1,7 @@
-import { FC } from "react";
-import { cn } from "@/lib/utils";
-import { Container } from "@/components/layout";
+import { FC } from 'react';
+import { cn } from '@/lib/utils';
+import { Container } from '@/components/layout';
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
 
 interface Props {
   className?: string;
@@ -8,13 +9,21 @@ interface Props {
 
 export const HomePartners: FC<Props> = ({ className }) => {
   return (
-    <section className={cn(className, "")}>
+    <section className={cn(className, '')}>
       <Container>
         <h2 className="h2 mb-8">Наши партнёры</h2>
 
-        <div className="w-[188px] h-20 bg-SURFACE_CONTAINER border border-CONTAINER_OUTLINE radius-[4px] py-4 px-[38px]">
-          <img src="/partner.svg" className="size-full object-cover" />
-        </div>
+        <Carousel>
+          <CarouselContent>
+            {[...Array(6)].map((_, i) => (
+              <CarouselItem
+                key={i}
+                className="basis-[188px] mr-8 h-20 bg-SURFACE_CONTAINER border border-CONTAINER_OUTLINE radius-[4px] py-4 px-[38px]">
+                <img src="/partner.svg" className="size-full object-cover" />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
       </Container>
     </section>
   );
