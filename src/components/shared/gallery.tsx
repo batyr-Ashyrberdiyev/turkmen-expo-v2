eedad32e805a65bb5eba476f01787772cc43b706
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-12-11 16:59:33 +0500

    padding changed

--- src/components/shared/gallery.tsx
+++ src/components/shared/gallery.tsx
@@ -1,15 +1,15 @@
-import { cn, scrollLock } from '@/lib/utils';
-import { FC } from 'react';
+import { cn, scrollLock } from "@/lib/utils";
+import { FC } from "react";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from '../ui/carousel';
-import { X } from 'lucide-react';
-import { GalleryStore } from '@/store/gallery';
-import { motion } from 'framer-motion';
+} from "../ui/carousel";
+import { X } from "lucide-react";
+import { GalleryStore } from "@/store/gallery";
+import { motion } from "framer-motion";
 
 interface Props {
   className?: string;
@@ -26,13 +26,18 @@ export const Gallery: FC<Props> = ({ className }) => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
-        className="size-full fixed top-0 left-0 bottom-0 right-0 z-[60]  overflow-y-auto bg-ON_SURFACE">
-        <Carousel className={cn('relative size-full', className)}>
+        className="size-full fixed top-0 left-0 bottom-0 right-0 z-[60]  overflow-y-auto bg-ON_SURFACE"
+      >
+        <Carousel className={cn("relative size-full", className)}>
           <div className="h-full flex flex-col justify-center">
             <CarouselContent className="size-full mx-auto">
               {[...Array(5)].map((_, i) => (
-                <CarouselItem key={i} className="py-40 px-[600px]">
-                  <img src="/media-1.png" alt="gallery-image" className="size-full object-cover" />
+                <CarouselItem key={i} className="py-[10%] px-[35%]">
+                  <img
+                    src="/media-1.png"
+                    alt="gallery-image"
+                    className="size-full object-cover"
+                  />
                 </CarouselItem>
               ))}
             </CarouselContent>
@@ -40,7 +45,8 @@ export const Gallery: FC<Props> = ({ className }) => {
 
           <button
             onClick={() => setIsOpen(false)}
-            className="size-12 absolute top-12 right-14 p-1 hover:scale-[1.2] transition-transform ease-in-out duration-200">
+            className="size-12 absolute top-12 right-14 p-1 hover:scale-[1.2] transition-transform ease-in-out duration-200"
+          >
             <X className="size-full text-white" />
           </button>
 
