f41083de3b1657d2789bb6629299131ae916ee0a
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-12-11 16:10:27 +0500

    viewport once added

--- src/components/layout/page-hero.tsx
+++ src/components/layout/page-hero.tsx
@@ -14,10 +14,13 @@ export const PageHero: FC<Props> = ({ className, img, title }) => {
       initial={{ y: 50, opacity: 0 }}
       whileInView={{ y: 0, opacity: 1 }}
       transition={{ ease: [0.1, 0, 0.2, 1], duration: 0.4 }}
+      viewport={{ once: true }}
       className={cn(className, 'relative w-full md:h-[328px] h-[232px]')}>
       <div className="absolute top-0 left-0 size-full bg-[#171D1B]/[16%] z-10" />
       <img src={img} className="absolute top-0 left-0 size-full object-cover" />
-      <h1 className="h1 position-center z-20 text-center w-full px-4 text-white">{title}</h1>
+      <h1 className="h1 position-center z-20 text-center w-full px-4 text-white max-w-[1000px]">
+        {title}
+      </h1>
     </motion.section>
   );
 };
